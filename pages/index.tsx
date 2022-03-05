04b9287c251f80e02d4e5b2f74c8b6c3fb0315de
Jaewon <sorhd134@gmail.com>
2022-03-05 18:04:18 +0900

    Prettier on TailwindCSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-20 grid gap-5 min-h-screen">
-      <div className="bg-white p-7 rounded-3xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
-        <div className="flex justify-between my-2">
+    <div className="grid min-h-screen gap-5 bg-slate-400 py-20 px-20">
+      <div className="rounded-3xl bg-white p-7 shadow-xl">
+        <span className="text-3xl font-semibold">Select Item</span>
+        <div className="my-2 flex justify-between">
           <span className="text-gray-500">Grey Chair</span>
           <span className="font-bold">$19</span>
         </div>
@@ -13,67 +13,67 @@ const Home: NextPage = () => {
           <span className="text-gray-500">Tooly Table</span>
           <span className="font-bold">$19</span>
         </div>
-        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+        <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
           <span>Total</span>
           <span className="font-bold">$38</span>
         </div>
-        <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto hover:bg-blue-700 hover:text-black transition-colors duration-200 active:bg-yellow-500 focus:bg-red-500">
+        <button className="mx-auto mt-5 w-1/2 rounded-xl bg-blue-500 p-3 text-center text-white transition-colors duration-200 hover:bg-blue-700 hover:text-black focus:bg-red-500 active:bg-yellow-500">
           Checkout
         </button>
       </div>
-      <div className="bg-white rounded-3xl shadow-xl overflow-hidden">
+      <div className="overflow-hidden rounded-3xl bg-white shadow-xl">
         <div className="bg-blue-500 p-6 pb-14">
-          <span className="text-white text-2xl">Profile</span>
+          <span className="text-2xl text-white">Profile</span>
         </div>
-        <div className="rounded-3xl p-6 relative -top-5 bg-white">
-          <div className="flex relative -top-16 justify-between items-end">
+        <div className="relative -top-5 rounded-3xl bg-white p-6">
+          <div className="relative -top-16 flex items-end justify-between">
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="h-24 w-24 rounded-full bg-red-400" />
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Spent</span>
               <span className="font-medium">$2,310</span>
             </div>
           </div>
-          <div className="relative  flex flex-col items-center -mt-10 -mb-5">
+          <div className="relative  -mt-10 -mb-5 flex flex-col items-center">
             <span className="text-lg font-medium">Tony Molloy</span>
             <span className="text-sm text-gray-500">미국</span>
           </div>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-3xl shadow-xl">
-        <div className="flex justify-between items-center mb-5">
+      <div className="rounded-3xl bg-white p-10 shadow-xl">
+        <div className="mb-5 flex items-center justify-between">
           <span>←</span>
           <div className="space-x-3">
             <span>⭐️ 4.9</span>
-            <span className="p-2 rounded-md shadow-xl">❤️</span>
+            <span className="rounded-md p-2 shadow-xl">❤️</span>
           </div>
         </div>
-        <div className="bg-zinc-400 h-72 mb-5" />
+        <div className="mb-5 h-72 bg-zinc-400" />
         <div className="flex flex-col">
-          <span className="font-medium mb-1 text-xl">Swoon Lounge</span>
+          <span className="mb-1 text-xl font-medium">Swoon Lounge</span>
           <span className="text-xs text-gray-500">Chair</span>
-          <div className="flex justify-between items-center mt-3 mb-5">
+          <div className="mt-3 mb-5 flex items-center justify-between">
             <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-500"></button>
-              <button className="w-5 h-5 rounded-full bg-indigo-500"></button>
-              <button className="w-5 h-5 rounded-full bg-teal-500"></button>
+              <button className="h-5 w-5 rounded-full bg-yellow-500"></button>
+              <button className="h-5 w-5 rounded-full bg-indigo-500"></button>
+              <button className="h-5 w-5 rounded-full bg-teal-500"></button>
             </div>
             <div className="flex items-center space-x-4">
-              <button className="bg-blue-100 rounded-lg flex justify-center items-center aspect-square w-10 text-gray-500 text-2xl">
+              <button className="flex aspect-square w-10 items-center justify-center rounded-lg bg-blue-100 text-2xl text-gray-500">
                 -
               </button>
-              <span className="font-bold text-xl">1</span>
-              <button className="bg-blue-100 rounded-lg flex justify-center items-center aspect-square w-10 text-gray-500 text-2xl">
+              <span className="text-xl font-bold">1</span>
+              <button className="flex aspect-square w-10 items-center justify-center rounded-lg bg-blue-100 text-2xl text-gray-500">
                 +
               </button>
             </div>
           </div>
-          <div className="flex justify-between items-center">
-            <span className="font-medium text-3xl">$450</span>
-            <button className="bg-blue-500 text-white rounded-xl px-8 py-3 text-sm">
+          <div className="flex items-center justify-between">
+            <span className="text-3xl font-medium">$450</span>
+            <button className="rounded-xl bg-blue-500 px-8 py-3 text-sm text-white">
               Add to cart
             </button>
           </div>
