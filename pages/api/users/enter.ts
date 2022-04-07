1827299ba72adfaafead8900230fa255cabd9359
Jaewon <sorhd134@gmail.com>
2022-04-07 20:40:32 +0900

    Sending Email

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,9 +1,11 @@
 import twilio from "twilio";
+import mail from "@sendgrid/mail";
 import client from "@libs/server/client";
 import withHandler, { Response } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+mail.setApiKey(process.env.SG_API_KEY!);
 
 async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
   const { phone, email } = req.body;
@@ -32,6 +34,14 @@ async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
     });
     console.log(message);
   } else {
+    const email = await mail.send({
+      from: "sorhd134@gmail.com",
+      to: "sorhd134@gmail.com",
+      subject: "Your Carrot Market Verification Email.",
+      text: `Your token is ${payload}.`,
+      html: `Your token is ${payload}.`,
+    });
+    console.log(email);
   }
   res.status(200).json({ ok: true });
 }
