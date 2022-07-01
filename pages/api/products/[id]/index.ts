2756acbf53c7c848efab14086d8f70a18b0f5ffc
Jaewon <sorhd134@gmail.com>
2022-07-01 16:05:37 +0900

    Favorite Products part Two

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -4,7 +4,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import withSession from "@libs/server/withSession";
 
 async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
-  const { id } = req.query;
+  const {
+    query: { id },
+    session: { user },
+  } = req;
   const product = await client.product.findUnique({
     where: { id: +(id as string).toString() },
     include: { user: { select: { id: true, avatar: true, name: true } } },
@@ -15,7 +18,13 @@ async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
   const relatedProducts = await client.product.findMany({
     where: { OR: terms, AND: [{ id: { not: product?.id } }] },
   });
-  res.status(200).json({ ok: true, product, relatedProducts });
+  const isLiked = Boolean(
+    await client.fav.findFirst({
+      where: { productId: product?.id, userId: user?.id },
+      select: { id: true },
+    })
+  );
+  res.status(200).json({ ok: true, product, relatedProducts, isLiked });
 }
 
 export default withSession(withHandler({ methods: ["GET"], handler }));
