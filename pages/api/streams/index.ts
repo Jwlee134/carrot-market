5034f588d6fb3045c3e7f3c854f4765c60b5ae89
Jaewon <sorhd134@gmail.com>
2022-07-14 21:27:23 +0900

    Upload Form

---
+++ pages/api/streams/index.ts
@@ -0,0 +1,10 @@
+import client from "@libs/server/client";
+import withHandler, { Response } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import withSession from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
+  res.status(200).json({ ok: true });
+}
+
+export default withSession(withHandler({ methods: ["POST"], handler }));
--- pages/streams/create.tsx
+++ pages/streams/create.tsx
@@ -3,16 +3,57 @@ import Button from "@components/Button";
 import Input from "@components/Input";
 import Layout from "@components/Layout";
 import Textarea from "@components/Textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { Stream } from "@prisma/client";
+
+interface Form {
+  name: string;
+  price: string;
+  description: string;
+}
 
 const Create: NextPage = () => {
+  const { register, handleSubmit } = useForm<Form>();
+  const [create, { data, loading }] = useMutation<{
+    ok: boolean;
+    stream: Stream;
+  }>("/streams");
+  const router = useRouter();
+
+  const onValid = (data: Form) => {
+    if (loading) return;
+    create(data);
+  };
+
+  useEffect(() => {
+    if (data?.ok) router.push(`/streams/${data.stream.id}`);
+  }, [data, router]);
+
   return (
     <Layout canGoBack>
-      <div className="space-y-5 px-4 py-10">
-        <Input kind="text" label="Name" name="name" />
-        <Input kind="price" label="Price" name="price" />
-        <Textarea label="Description" name="description" />
-        <Button text="Create stream" />
-      </div>
+      <form className="space-y-5 px-4 py-10" onSubmit={handleSubmit(onValid)}>
+        <Input
+          register={register("name", { required: true })}
+          kind="text"
+          label="Name"
+          name="name"
+        />
+        <Input
+          register={register("price", { required: true })}
+          kind="price"
+          label="Price"
+          name="price"
+        />
+        <Textarea
+          register={register("description", { required: true })}
+          label="Description"
+          name="description"
+        />
+        <Button text={loading ? "Loading" : "Create stream"} />
+      </form>
     </Layout>
   );
 };
